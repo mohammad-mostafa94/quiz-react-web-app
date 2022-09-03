58d3a473dd8d834ae89eb5f8a95612a69199a68b
mohammad-mostafa94 <mohammadmostafa1094@gmail.com>
2022-09-03 21:36:20 +0600

    mdi icon added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Helmet from 'react-helmet';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -9,6 +10,16 @@ const Home = () => {
                 <section>
                     <div>
                         <span className="mdi mdi-cube-outline mdi-48px"></span>
+                    </div>
+                    <h1>Quiz App</h1>
+                    <div className="play-button-container">
+                        <ul>
+                            <li><Link to="/play/instructions">Play</Link></li>
+                        </ul>
+                    </div>
+                    <div className="auth-container">
+                        <Link to="login">Login</Link>
+                        <Link to="/register">Register</Link>
                     </div>
                 </section>
             </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import App from './App';
+import "mdi-icons/css/materialdesignicons.min.css";
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
